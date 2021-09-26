be1abaecb8a0e95f98de2e1e66ba9a18ad5b55fb
Michael Bayucot <mbayucot@gmail.com>
2021-09-26 15:21:00 +0800

    Add article query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,15 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :articles, Types::ArticleType.collection_type, null: true do
+      argument :page, Integer, required: false
+      argument :limit, Integer, required: false
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def articles(page: nil, limit: nil)
+      ::Article.page(page).per(limit)
     end
   end
 end
