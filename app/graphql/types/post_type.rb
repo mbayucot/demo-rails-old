830b939e8fd39ea6f0501777fbb82442381a5a69
Michael Bayucot <mbayucot@gmail.com>
2021-12-11 09:56:44 +0800

    Update graphql schema

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,8 +3,8 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :body, String, null: false
-    #field :created_at, GraphQL::Types::ISO8601DateTime, null: true
-    #field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
     field :created_at, String, null: true
     field :updated_at, String, null: true
     field :slug, String, null: true
